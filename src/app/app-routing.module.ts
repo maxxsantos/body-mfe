fa4ce3b7b92d8f43b92bf18d8e865b788003b2c2
Max <max.santos@icomp.ufam.edu.br>
2022-03-03 20:48:49 -0400

    body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('header/Module').then(
+        m => m.RemoteHeaderModule
+        ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
